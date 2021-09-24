c995847a82d9ca47244bf775577059c1358964c7
Lauras Dilys <lauras.dilys@gmail.com>
2021-09-24 15:38:10 +0300

    namespace API => Api

--- Api/Controllers/LogController.cs
+++ Api/Controllers/LogController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API.Controllers
+namespace Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
