0f209dcdfb7780f7e6a9756ffa83c7a10d4710e8
Lauras Dilys <lauras.dilys@gmail.com>
2021-09-24 16:19:58 +0300

    Added project references

---
+++ Api/StartupExtentions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Api
+{
+    public static class StartupExtentions
+    {
+        public static IServiceCollection CongigureDependencyInjection(this IServiceCollection services)
+        {
+            //services.AddScoped<IObject, ActualObject>();
+
+            return services;
+        }
+    }
+}
