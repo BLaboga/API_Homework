155f62e835d9add92db8971da5739f6ef76c176d
Lauras Dilys <lauras.dilys@gmail.com>
2021-09-24 15:29:50 +0300

    Added project layers

---
+++ Api/Controllers/LogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogController : ControllerBase
+    {
+        public LogController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
